c383863dfc926d5adba4222cb4d43c978c113dc7
jsdeveloperr <jsdeveloperr@gmail.com>
2021-12-19 23:31:14 +0300

    locale

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -56,7 +56,6 @@ const Main = (props: IMainProps) => (
         <span role="img" aria-label="Love">
           ♥
         </span>{' '}
-        by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
         {/*
          * PLEASE READ THIS SECTION
          * We'll really appreciate if you could have a link to our website
